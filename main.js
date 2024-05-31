f481cdd34a4422c9c091778498aca624a698abe5
Anmol Chandel <anmolchandel944@gmail.com>
2024-05-31 14:17:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new typed(".text", {
+    strings:["frontend developer", "YouTuber", "web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
+
+
+
+  
